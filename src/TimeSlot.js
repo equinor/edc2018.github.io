b39c87712f24e03fab914119444353bd09d85b68
Árni Geir Úlfarsson <arnigeirulfarsson@Arnis-MacBook-Pro.local>
2018-09-15 00:56:34 +0200

    schedule

---
+++ src/TimeSlot.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  position: relative;
+  z-index: -2;
+  height: 140px;
+`;
+
+const Time = styled.h3`
+  // position: absolute;
+  // left: 0;
+  // right: 0;
+  // margin: auto;
+  // color: white;
+  // z-index: -1;
+  // font-size: 7em;
+  position: absolute;
+  /* left: -32px; */
+  /* right: 0; */
+  margin: auto;
+  padding-left: 100px;
+  color: white;
+  z-index: -1;
+  font-size: 7em;
+`;
+
+const TimeSlotHeading = styled.h4`
+  color: #ec384a;
+`;
+
+const Lectures = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const Lecture = styled.div`
+  padding: 0px 20px;
+`;
+
+const LectureName = styled.p``;
+
+const Lecturer = styled.p`
+  font-size: 0.8em;
+  font-style: oblique;
+  color: gray;
+  ::before {
+    content: '– ';
+  }
+`;
+
+const LeisureWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const Leisure = styled.p``;
+
+const TimeSlot = ({ time, heading, lectures, leisure }) => (
+  <Wrapper>
+    <Time>{time}</Time>
+    {heading && <TimeSlotHeading>{heading}</TimeSlotHeading>}
+    {lectures && (
+      <Lectures>
+        {lectures.map(({ name, lecturer }) => (
+          <Lecture>
+            <LectureName>{name}</LectureName>
+            {lecturer && <Lecturer>{lecturer}</Lecturer>}
+          </Lecture>
+        ))}
+      </Lectures>
+    )}
+    {leisure && (
+      <LeisureWrapper>
+        <Leisure>{leisure}</Leisure>
+      </LeisureWrapper>
+    )}
+  </Wrapper>
+);
+
+export default TimeSlot;
